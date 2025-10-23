a5a20dd55e51061e91a53bf3400d3d323c715db1
Badusha Salim <badhushacs88@gmail.com>
2025-10-23 15:36:43 +0530

    Time: 12 ms (7.12%), Space: 60.3 MB (5.18%) - LeetHub

--- 0389-find-the-difference/0389-find-the-difference.ts
+++ 0389-find-the-difference/0389-find-the-difference.ts
@@ -1,6 +1,6 @@
 function findTheDifference(s: string, t: string): string {
-    for(let letter of s){
-        t=t.replace(letter,'')
+    for (let letter of s) {
+        t = t.replace(letter, '')
     }
     return t
 };
