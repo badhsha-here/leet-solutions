a983b0cf9d5f82aeb0819126e6375d06bb2d0d25
Badusha Salim <badhushacs88@gmail.com>
2025-10-27 15:27:42 +0530

    Time: 68 ms (5.03%), Space: 62 MB (6.96%) - LeetHub

---
+++ 0283-move-zeroes/0283-move-zeroes.ts
@@ -0,0 +1,12 @@
+/**
+ Do not return anything, modify nums in-place instead.
+ */
+function moveZeroes(nums: number[]): void {
+    let x= 0;
+    for (let i = 0; i < nums.length; i++) {
+        if (nums[i] !== 0) {
+            [nums[x], nums[i]] = [nums[i], nums[x]];
+            x++
+        }
+    }
+};
