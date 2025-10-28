a260696d4c01cd1177849e71069d5f861bc0e460
Badusha Salim <badhushacs88@gmail.com>
2025-10-28 16:20:57 +0530

    Time: 0 ms (100%), Space: 55.8 MB (33.49%) - LeetHub

---
+++ 1542-consecutive-characters/1542-consecutive-characters.js
@@ -0,0 +1,18 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var maxPower = function (s) {
+    let max = 1;
+    let count = 1;
+    for (let i = 0; i < s.length; i++) {
+        if (s[i] === s[i - 1]) {
+            count++
+            max = Math.max(count, max)
+        }
+        else {
+            count = 1
+        }
+    }
+    return max
+};
