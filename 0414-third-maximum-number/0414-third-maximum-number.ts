0c30edb55b67feb13092332464519c7d201341ff
Badusha Salim <badhushacs88@gmail.com>
2025-11-15 13:05:15 +0530

    Time: 3 ms (65.27%), Space: 57.8 MB (64.89%) - LeetHub

---
+++ 0414-third-maximum-number/0414-third-maximum-number.ts
@@ -0,0 +1,9 @@
+function thirdMax(nums: number[]): number {
+    let set = new Set(nums);
+    let arr = [...set];
+    let result = arr.sort((a, b) => b - a);
+    if (result.length >= 3) {
+        return result[2]
+    }
+    return result[0]
+};
