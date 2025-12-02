ff8d8bf02169ede7776a668182eb6a74988eb28d
Badusha Salim <badhushacs88@gmail.com>
2025-12-02 10:55:19 +0530

    Time: 1 ms (24.96%), Space: 54.2 MB (46.32%) - LeetHub

---
+++ 0367-valid-perfect-square/0367-valid-perfect-square.js
@@ -0,0 +1,11 @@
+/**
+ * @param {number} num
+ * @return {boolean}
+ */
+var isPerfectSquare = function (num) {
+    if (num === 1) return true
+    for (let i = 0; i * i <= num; i++) {
+        if (i * i === num) return true
+    }
+    return false
+};
